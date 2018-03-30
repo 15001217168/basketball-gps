5d49124b618a4a7fd77202cbf450a58030f2eeb6
刘雪松 <1076239475@qq.com>
2018-03-30 15:11:44 +0800

    a

--- gps.js
+++ gps.js
@@ -13,6 +13,7 @@ var gps = {
         }
     },
     location: function(position) {
+        alert(JSON.stringify(position.coords));
         this.pos.latitude = position.coords.latitude;
         this.pos.longitude = position.coords.longitude;
         this.init_map();
